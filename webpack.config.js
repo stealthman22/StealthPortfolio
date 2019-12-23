a3a614d651c4cba7d85f367aa8be766ce8d93bcc
stealthman22 <tdotonline@gmail.com>
2019-12-23 14:23:55 +0000

    created config file

---
+++ webpack.config.js
@@ -0,0 +1,13 @@
+//require plugins
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+// modules
+module.exports = {
+    mode: 'development',
+    module: {
+        rules: [
+
+        ]
+    },
+}
