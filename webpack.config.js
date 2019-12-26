f5f93e64acdb256bfe37b0e7ea7d5a0cbf834c66
stealthman22 <tdotonline@gmail.com>
2019-12-26 20:51:24 +0000

    creating config file and ran first build

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,48 @@
 //require plugins
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const { CleanWebPackPlugin } = require('clean-webpack-plugin');
 
 // modules
 module.exports = {
     mode: 'development',
+    entry: {
+        app: './src/index.js',
+    },
+    devtool: 'eval',
+    devServer: {
+        contentBase: './dist'
+    },
     module: {
         rules: [
+            {
+                test: /\.html$/,
+                use: [
+                    {
+                        // load with html loader and then minimize
+                        loader: 'html-loader',
+                        options: { minimize: true }
+                    }
+                ]
+            },
+            {
+                test: /\.css$/,
+                use: ['style-loader', 'css-loader']
+            },
 
         ]
     },
+    plugins: [
+        new CleanWebPackPlugin({
+
+        }),
+        new HtmlWebpackPlugin({
+            title: 'Portfolio'
+        })
+    ],
+    output: {
+        filename: '[name].bundle.js',
+        path: path.resolve(__dirname, 'dist'),
+        publicPath: '/',
+    }
 }
