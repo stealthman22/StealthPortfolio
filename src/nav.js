c64e81da38422b8970f64e621a815d74b6a4e212
stealthman22 <tdotonline@gmail.com>
2020-03-11 17:19:50 +0000

    almost done nav-links acting up

--- src/nav.js
+++ src/nav.js
@@ -14,3 +14,28 @@ window.onscroll = function () {
 
 console.log(nav)
 console.log(prevScrollPos)
+
+// Responsive hamburger
+
+const hamburger = document.querySelector('.hamburger');
+const navUl = document.querySelector('.nav-ul');
+const links = document.querySelectorAll('.nav-ul li');
+const lineOne = document.querySelector('.hamburger .line:nth-child(1)');
+const lineTwo = document.querySelector('.hamburger .line:nth-child(2)');
+const lineThree = document.querySelector('.hamburger .line:nth-child(3)');
+
+hamburger.addEventListener('click', ()=> {
+    navUl.classList.toggle('open');
+    links.forEach(link =>link.classList.toggle('fade'));
+    lineThree.classList.toggle('transparent');
+    lineOne.classList.toggle('rotateOne');
+    lineTwo.classList.toggle('rotateTwo');
+})
+
+links.forEach(link=>link.addEventListener('click', ()=> {
+   nav.classList.add('transparent');
+  
+})) 
+
+
+
