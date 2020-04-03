d60c84334881859b5460063b643109402721b9a3
stealthman22 <tdotonline@gmail.com>
2020-04-03 18:43:19 +0000

    working on bg images logic

--- src/dynamicHero.js
+++ src/dynamicHero.js
@@ -50,31 +50,46 @@ function setBg() {
 
 
     // Randomize bg
+
     // morning
     let morningArray = ['sunset-view-of-mountains-733100.jpg', 'purple-petal-flower-surrounded-by-green-plants-during-66288.jpg', 'time-lapse-photography-of-waterfalls-during-sunset-210186.jpg', 'two-cargo-ships-sailing-near-city-2144905.jpg'];
     let morningBg = morningArray[Math.floor(Math.random() * morningArray.length)];
-    let morningPath = '../src/img/hero/';
+    let morningPath = './src/img/hero/';
     let morning = morningPath + morningBg
-    console.log(morning)
+ 
     // afternoon
-    let afternoonArray = ['empty-dining-tables-and-chairs-1579739.jpg', 'brown-and-green-mountain-view-photo-842711.jpg', 'photo-of-keyboard-near-mouse-3184460.jpg', 'america-arid-bushes-california-221148.jpg'];
+    afternoonArray = ['empty-dining-tables-and-chairs-1579739.jpg', 'brown-and-green-mountain-view-photo-842711.jpg', 'photo-of-keyboard-near-mouse-3184460.jpg', 'america-arid-bushes-california-221148.jpg'];
     let afternoonBg = afternoonArray[Math.floor(Math.random() * afternoonArray.length)];
-    let afternoonPath = '../src/img/hero/';
+    let afternoonPath = './src/img/hero/';
     let afternoon = afternoonPath + afternoonBg;
 
     // evening
-    let eveningArray = ['twisted-building-during-nighttime-1470405.jpg', 'beautiful-beauty-blue-bright-414612.jpg', 'landscape-photo-of-mountain-with-polar-lights-1434608.jpg', 'photo-of-toronto-cityscape-at-night-2478248.jpg'];
+    let eveningArray = [ 'twisted-building-during-nighttime-1470405.jpg', 'beautiful-beauty-blue-bright-414612.jpg', 'landscape-photo-of-mountain-with-polar-lights-1434608.jpg', 'photo-of-toronto-cityscape-at-night-2478248.jpg'];
     let eveningBg = eveningArray[Math.floor(Math.random() * eveningArray.length)];
     let eveningPath = './src/img/hero/';
     let evening = eveningPath + eveningBg;
 
-// Set media queries
-// if (window.matchMedia("(max-width:1024px)").matches) {
-//    let  eveningArray = ['agarthaa.jpg'];
-//     let eveningBg = eveningArray[Math.floor(Math.random() * eveningArray.length)];
-//     let eveningPath = './src/img/hero/';
-//     let evening = eveningPath + eveningBg;
-// }
+    function tabQuery(x) {
+        if (x.matches) {
+            afternoonArray = ['agarthaa.jpg'];
+            eveningArray = ['agarthaa.jpg'];
+        }
+        }
+        const x = window.matchMedia('(max-width:1024px)');
+        tabQuery(x)
+        x.addListener(tabQuery);
+        console.log(x)
+
+        // // bg-images for mobile
+        // function mobileQuery(y) {
+        //     if (y.matches) {
+        //         afternoonArray = ['kyere.png']
+        //     }
+        // }
+        // const y = window.matchMedia('(max-width:600px)');
+        // mobileQuery(y);
+        // y.addListener( mobileQuery)
+        // console.log(y)
 
     // Show bg
     //morning
@@ -95,6 +110,12 @@ function setBg() {
         greetingJs.textContent = 'Hi Good Evening,'
     }
 
+      // Set media queries
+//   if (window.matchMedia("(max-width:1024px)").matches) {
+//     morningArray = ;
+//     console.log(morningArray)
+// }
+    
 }
 
 
@@ -104,5 +125,6 @@ console.log(images)
 getTime();
 addZero();
 setBg();
+
 
 
