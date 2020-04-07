79a8a0c5e214989904cf5e6707849eeee55d5894
stealthman22 <tdotonline@gmail.com>
2020-04-07 01:30:18 +0000

    matchmedia working images compressed in hero section

--- src/dynamicHero.js
+++ src/dynamicHero.js
@@ -56,24 +56,42 @@ function showBg() {
     // Randomize bg
 // morning
     let morningArray = ['sunset-view-of-mountains-733100.jpg', 'purple-petal-flower-surrounded-by-green-plants-during-66288.jpg', 'time-lapse-photography-of-waterfalls-during-sunset-210186.jpg', 'two-cargo-ships-sailing-near-city-2144905.jpg'];
+    if (window.matchMedia("(max-width:1024px)").matches) {
+    morningArray = ['sunset-view-of-mountains-733100-m.jpg', 'purple-petal-flower-surrounded-by-green-plants-during-66288-m.jpg', 'time-lapse-photography-of-waterfalls-during-sunset-210186-m.jpg', 'two-cargo-ships-sailing-near-city-2144905-m.jpg'];
+        console.log(morningArray)
+    }
+    if (window.matchMedia("(max-width:600px)").matches) {
+        morningArray = ['sunset-view-of-mountains-733100-s.jpg', 'purple-petal-flower-surrounded-by-green-plants-during-66288-s.jpg', 'time-lapse-photography-of-waterfalls-during-sunset-210186-s.jpg', 'two-cargo-ships-sailing-near-city-2144905-s.jpg'];
+        console.log(morningArray)
+    }
     let morningBg = morningArray[Math.floor(Math.random() * morningArray.length)];
     let morningPath = './src/img/hero/';
     let morning = morningPath + morningBg
 
     // afternoon
-   let afternoonArray = ['empty-dining-tables-and-chairs-1579739.jpg', 'brown-and-green-mountain-view-photo-842711.jpg', 'photo-of-keyboard-near-mouse-3184460.jpg', 'america-arid-bushes-california-221148.jpg'];
-    let afternoonBg = afternoonArray[Math.floor(Math.random() * afternoonArray.length)];
+   let afternoonArray = ['brown-trench-coat-on-wooden-rack-821357.jpg', 'brown-and-green-mountain-view-photo-842711.jpg', 'photo-of-keyboard-near-mouse-3184460.jpg', 'america-arid-bushes-california-221148.jpg'];
+   if (window.matchMedia("(max-width:1024px)").matches) {
+    afternoonArray = ['brown-trench-coat-on-wooden-rack-821357-m.jpg', 'brown-and-green-mountain-view-photo-842711-m.jpg', 'photo-of-keyboard-near-mouse-3184460-m.jpg', 'america-arid-bushes-california-221148-m.jpg'];
+    console.log(afternoonArray)
+}
+if (window.matchMedia("(max-width:600px)").matches) {
+   afternoonArray = ['brown-trench-coat-on-wooden-rack-821357.jpg-s', 'brown-and-green-mountain-view-photo-842711-s.jpg', 'photo-of-keyboard-near-mouse-3184460-s.jpg', 'america-arid-bushes-california-221148-s.jpg'];
+    console.log(afternoonArray)
+}
+   let afternoonBg = afternoonArray[Math.floor(Math.random() * afternoonArray.length)];
     let afternoonPath = './src/img/hero/';
     let afternoon = afternoonPath + afternoonBg;
 
     // evening
     let eveningArray = [ 'twisted-building-during-nighttime-1470405.jpg', 'beautiful-beauty-blue-bright-414612.jpg', 'landscape-photo-of-mountain-with-polar-lights-1434608.jpg', 'photo-of-toronto-cityscape-at-night-2478248.jpg'];
-    
     if (window.matchMedia("(max-width:1024px)").matches) {
-        eveningArray = ['Talon.png'];
+        eveningArray = [ 'twisted-building-during-nighttime-1470405-m.jpg', 'beautiful-beauty-blue-bright-414612-m.jpg', 'landscape-photo-of-mountain-with-polar-lights-1434608-m.jpg', 'photo-of-toronto-cityscape-at-night-2478248-m.jpg'];
         console.log(eveningArray)
     }
-    
+    if (window.matchMedia("(max-width:600px)").matches) {
+        eveningArray = [ 'twisted-building-during-nighttime-1470405-s.jpg', 'beautiful-beauty-blue-bright-414612-s.jpg', 'landscape-photo-of-mountain-with-polar-lights-1434608-s.jpg', 'photo-of-toronto-cityscape-at-night-2478248-s.jpg'];
+        console.log(eveningArray)
+    }
     let eveningBg = eveningArray[Math.floor(Math.random() * eveningArray.length)];
     let eveningPath = './src/img/hero/';
     let evening = eveningPath + eveningBg;
